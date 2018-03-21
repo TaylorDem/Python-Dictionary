26335b486e66e4173c85a84af3f4922ee2a35774
Mr. Fix-it <demarchet@yahoo.com>
2018-03-21 19:19:01 -0400

    Trying some things out.
    Learning as I go. Right now it just trolls you. So. Worth.

--- dict.py
+++ dict.py
@@ -6,4 +6,14 @@ import tkinter
 top = tkinter.Tk()
 
 # Code to add widgets will go here...
+
+def alertMe():
+    alertWindow = tkinter.Tk()
+    alertText = tkinter.Text(alertWindow)
+    alertText.insert(tkinter.INSERT, "Why did you think this was real?")
+    alertText.pack()
+    alertWindow.mainloop()
+
+UselessAlert = tkinter.Button(top, text="Alert", command=alertMe)
+UselessAlert.pack()
 top.mainloop()
