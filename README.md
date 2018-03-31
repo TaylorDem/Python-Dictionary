5d5d5e2c06168d702b4bf000626af5c577126da4
Mr. Fix-it <demarchet@yahoo.com>
2018-03-31 00:33:04 -0400

    Can now pull Dictionary.com Definintions
    now using modules request and beautifulsoup4 to pull from dictionary.com and display the definition. big step right here in my opinion.

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 taking idea from C++ project I have and making it in python with a gui for practice coding.
 
 
-right now I can save words to a list that pops up on the right side with a scrollbar.
+right now I can save words to a list that pops up on the right side with a scrollbar. 
+delete them with a confirmation box and I am able to pull dictionary.com definitions using search button.
 
 next steps:
-save words for good (most likely in just a .txt file to start).
-delete words for good obvi
-actually have definitions pop up/saved.
+save/delete words for good (most likely in just a .txt file to start).
+have option to create custom definitions and save them.
 create menu with commands like sort or change color
 
 
-long term:
-be able to search for word matchup on dictionary.com + fill in definition from the website.
+other goals:
 be able to multi-delete words in list.
--- dict.py
+++ dict.py
@@ -3,14 +3,20 @@
 #   pyinstaller dict.py in terminal will create a .exe
 #in case its been far too long.. python dict.py runs program
 #   
+
+#These imports handle GUI
 import tkinter
 from tkinter import messagebox
+
+#These imports will handle pulling from dictionary.com
+import requests
+import bs4  #beautifulsoup4
 
 #top is the window that pops up.
 top = tkinter.Tk()
 top.title("Taylor Dictionary")
 top.configure(bg='blue')
-top.geometry('350x200') #size of window
+top.geometry('500x300') #size of window
 
 #need frames to make it pretty and organized
 leftFrame = tkinter.Frame(top)
@@ -20,18 +26,25 @@ leftFrame.pack(side = tkinter.LEFT)
 rightFrame = tkinter.Frame(top)
 rightFrame.pack(side=tkinter.RIGHT)
 
+
 #scrollbar is for my list of saved words
 scrollbar = tkinter.Scrollbar(rightFrame)
 scrollbar.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 
 #savedText is the text box/list of saved words obvi
 savedText = tkinter.Listbox(rightFrame, yscrollcommand = scrollbar.set)
+savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
+
+#defined is the text box that hold definition
+definitionBox = tkinter.Text(rightFrame)
+definitionBox.insert(tkinter.INSERT, 'Definition here.')
+definitionBox.pack(side = tkinter.RIGHT)
 
 #this function is the save button command
 def saveWord():
-    if messagebox.askokcancel("Confirm", "You sure this is a word?"):
+    #if messagebox.askokcancel("Confirm", "You sure this is a word?"): #I don't think I like this.
         savedText.insert(savedText.size(), wordEntry.get())
-        savedText.pack(anchor = tkinter.NE, fill = tkinter.BOTH)
+        savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
         wordEntry.delete(0, tkinter.END)
 
 #this line makes the scrollbar actually work, the rest is build up/initializing   
@@ -59,10 +72,28 @@ saveButton.pack(anchor = tkinter.W)
 def deleteWord():
     if messagebox.askokcancel("Confirm", "You sure this isn't a word?"):
         savedText.delete(savedText.curselection())
-        savedText.pack(anchor = tkinter.NE, fill = tkinter.BOTH)
+        savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
 
 #delete button
 deleteButton = tkinter.Button(leftFrame, text="Delete", command=deleteWord)
 deleteButton.pack(anchor = tkinter.W)
+
+#search function
+def searchOnline():
+    dictionaryurl = 'http://www.dictionary.com/browse/' + savedText.get(savedText.curselection()) + '?s=t'
+    searchWord = requests.get(dictionaryurl)
+    definitionBox.delete(1.0, tkinter.END) #it was a hassle here. 1.0 is very important!
+    #this line below makes it legible
+    parsedText = bs4.BeautifulSoup(searchWord.text, 'html.parser')
+    #the site dictionary.com has the definition in the 'meta' tag on its site with the name description 
+    definition = parsedText.find('meta', attrs={'name':'description'})
+    definitionBox.insert(tkinter.INSERT, definition["content"][:-10])   #this pulls content out of it.args
+    #each content on the site ends with " see more." so with   ^^^^ I delete the last 10 characters
+    definitionBox.pack(side = tkinter.BOTTOM)
+
+#search button :)
+searchButton = tkinter.Button(leftFrame, text="Online Def", command=searchOnline)
+searchButton.pack(anchor = tkinter.W)
+
 
 top.mainloop()
