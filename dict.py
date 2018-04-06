10c9308ef9b81b79ee233474910b7e51d146141d
Mr. Fix-it <demarchet@yahoo.com>
2018-04-06 00:10:15 -0400

    Add delete, duplicate check, and ABC
    we have handling of duplicates. including an error message that shows in red.
    
    everything saves to a .txt file and deletes correctly and loads previous saved words
    
    now the list is also alphabetized as words are saved.

--- dict.py
+++ dict.py
@@ -1,7 +1,7 @@
 #Dictionary Project using Tkinter within python
 #
 #   pyinstaller dict.py in terminal will create a .exe
-#in case its been far too long.. python dict.py runs program
+#   in case its been far too long.. python dict.py runs program
 #   
 
 #These imports handle GUI
@@ -26,13 +26,19 @@ leftFrame.pack(side = tkinter.LEFT)
 rightFrame = tkinter.Frame(top)
 rightFrame.pack(side=tkinter.RIGHT)
 
-
 #scrollbar is for my list of saved words
 scrollbar = tkinter.Scrollbar(rightFrame)
 scrollbar.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 
 #savedText is the text box/list of saved words obvi
 savedText = tkinter.Listbox(rightFrame, yscrollcommand = scrollbar.set)
+#start with previous list still there.
+text_file = open("Current_Dict.txt","r")
+old_words = text_file.readlines()
+for lines in old_words:
+    savedText.insert(savedText.size(), lines[:-1])
+text_file.close()
+
 savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
 
 #defined is the text box that hold definition
@@ -41,20 +47,48 @@ definitionBox.config(wrap=tkinter.WORD)
 definitionBox.insert(tkinter.INSERT, 'Definition here.')
 definitionBox.pack(side = tkinter.RIGHT)
 
-#opens .txt file to save more permanently
-text_file = open("Current_Dict.txt", "a")
+#alphabetize function
+def sortAZ():
+    #pull whole list
+    temp_list = list(savedText.get(0, tkinter.END))
+    #apparently sorting is inherent with lists
+    temp_list.sort(key=str.lower)
+    #delete contents of present listbox
+    savedText.delete(0, tkinter.END)
+    #load listbox with sorted data
+    for item in temp_list:
+        savedText.insert(tkinter.END, item)
 
 #this function is the save button command
 def saveWord():
     #if messagebox.askokcancel("Confirm", "You sure this is a word?"): #I don't think I like this. at all.
+    #i wanna check to see if the word is already there.
+    dupe = False
+    for x in range(0, savedText.size()):
+        if savedText.get(x) == wordEntry.get():
+            dupe = True
+
+    if dupe == False: #if not already there save
         savedText.insert(savedText.size(), wordEntry.get())
+        sortAZ()
         savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
         
-        #
-        #Now this bit will save word to a .txt file
-        text_file.write("%s\n" % (wordEntry.get()))
+        #opens .txt file to save
+        text_file = open("Current_Dict.txt", "w") #a lets you append instead
+        #text_file.write("%s\n" % (wordEntry.get())
+        #Now this bit will save word list to a .txt file
+        for x in range(0, savedText.size()):
+            text_file.write("%s\n" % (savedText.get(x)))
 
-        wordEntry.delete(0, tkinter.END) #clear it for next word
+        text_file.close()
+
+    if dupe == True: #show error message in definition box for now
+        definitionBox.delete(1.0, tkinter.END)
+        definitionBox.config(fg = 'red')
+        definitionBox.insert(tkinter.INSERT, "That word already exists in the dictionary!")
+        definitionBox.pack(side = tkinter.BOTTOM)
+
+    wordEntry.delete(0, tkinter.END) #clear entry box for next word
 
 #this line makes the scrollbar actually work, the rest is build up/initializing   
 scrollbar.config(command = savedText.yview)
@@ -80,6 +114,21 @@ saveButton.pack(anchor = tkinter.W)
 #delete function
 def deleteWord():
     if messagebox.askokcancel("Confirm", "You sure this isn't a word?"):
+        markedWord = savedText.get(savedText.curselection()) #put the word here for legibility
+
+        #this first bit here opens and reads the file
+        text_file = open("Current_Dict.txt","r")
+        words = text_file.readlines()
+        text_file.close()
+
+        #now rewriting file to contain every line except the deleted one
+        text_file = open("Current_Dict.txt", "w")
+        for line in words:
+            if line!= markedWord+"\n":
+                text_file.write(line)
+        text_file.close()
+
+        #now delete the visual one
         savedText.delete(savedText.curselection())
         savedText.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
 
@@ -91,6 +140,7 @@ deleteButton.pack(anchor = tkinter.W)
 def searchOnline():
     dictionaryurl = 'http://www.dictionary.com/browse/' + savedText.get(savedText.curselection()) + '?s=t'
     searchWord = requests.get(dictionaryurl)
+    #this line clears old text
     definitionBox.delete(1.0, tkinter.END) #it was a hassle here. 1.0 is very important!
     #this line below makes it legible
     parsedText = bs4.BeautifulSoup(searchWord.text, 'html.parser')
@@ -98,11 +148,11 @@ def searchOnline():
     definition = parsedText.find('meta', attrs={'name':'description'})
     definitionBox.insert(tkinter.INSERT, definition["content"][:-10])   #this pulls content out of it.args
     #each content on the site ends with " see more." so with   ^^^^ I delete the last 10 characters
+    definitionBox.config(fg='black')
     definitionBox.pack(side = tkinter.BOTTOM)
 
 #search button :)
 searchButton = tkinter.Button(leftFrame, text="Online Def", command=searchOnline)
 searchButton.pack(anchor = tkinter.W)
-
 
 top.mainloop()
